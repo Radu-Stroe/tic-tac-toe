8a2632331f254c5c2a1203795d9457d97a70d5ee
Radu <stroeradu03@gmail.com>
2022-01-03 17:29:54 +0200

    Restart button implementation

--- script.js
+++ script.js
@@ -1,46 +1,73 @@
 const board = document.querySelector('#board');
+const restart = document.querySelector('.restart');
 
-let gameBoard = (() => {
-    let array = [
-        {marked: false, symbol: 'X'}, 
-        {marked: false, symbol: ''}, 
-        {marked: false, symbol: 'X'}, 
-        {marked: false, symbol: ''}, 
-        {marked: false, symbol: 'X'}, 
-        {marked: false, symbol: ''}, 
-        {marked: false, symbol: ''}, 
-        {marked: false, symbol: 'X'}, 
-        {marked: false, symbol: ''}
-    ];
-    array.forEach((element) => {
-        let div = document.createElement('div');
-        div.textContent = element.symbol;
-        div.classList.add('boardElement');
-        board.appendChild(div);
-    })
-    return { array }; 
-})();
+let playerFlag = true;
 
-const gameBoardLocalStorage = (() => {
-    const saveToLocalStorage = () => {
-        localStorage.setItem('gameBoard', JSON.stringify(gameBoard));
-        window.location = window.location;
-      }
+//LOCAL STORAGE
+// const gameBoardLocalStorage = (() => {
+//     const saveToLocalStorage = () => {
+//         localStorage.setItem('gameBoard', JSON.stringify(gameBoard.array));
+//         window.location = window.location;
+//       }
       
-    const loadFromLocalStorage = () => {
-        // if(localStorage.getItem('gameBoard') === null) {
-        //     library.books = [];
-        // } else {
-            // library.books = [...JSON.parse(localStorage.getItem('library'))];
-        // }
-        gameBoard.array = JSON.parse(localStorage.getItem('gameBoard'));
-    };
-    return { saveToLocalStorage, loadFromLocalStorage};
-})();
+//     const loadFromLocalStorage = () => {
+//         if(localStorage.getItem('gameBoard') != null) {
+//             console.log(gameBoard.array)
+//             gameBoard.array = JSON.parse(localStorage.getItem('gameBoard')).array;
+//             console.log(gameBoard.array)
+//         }
+//     };
+//     return { saveToLocalStorage, loadFromLocalStorage};
+// })();
 
-let player = (name, symbol) => {
-    return { name, symbol }
+//PLAYERS
+let player = (symbol) => {
+    return {symbol }
 };
 
-let firstPlayer = player('Dorel', 'X');
-let secondPlayer = player('Gigel', 'O');
+let firstPlayer = player('X');
+let secondPlayer = player('O');
+
+//GAMEBOARD
+let gameBoard = (() => {
+    let array = [
+        {symbol: ''}, 
+        {symbol: ''}, 
+        {symbol: ''}, 
+        {symbol: ''}, 
+        {symbol: ''}, 
+        {symbol: ''}, 
+        {symbol: ''}, 
+        {symbol: ''}, 
+        {symbol: ''}
+    ];
+    const renderGameBoard = () => {
+        array.forEach((element) => {
+            let div = document.createElement('div');
+            div.classList.add('boardElement');
+            div.addEventListener('click', () => {
+                if(playerFlag) {
+                    div.textContent = firstPlayer.symbol;
+                    array[array.indexOf(element)].symbol = firstPlayer.symbol;
+                    playerFlag = false;
+                } else {
+                    div.textContent = secondPlayer.symbol;
+                    array[array.indexOf(element)].symbol = secondPlayer.symbol;
+                    playerFlag = true;
+                }
+            }, {once : true})
+            board.appendChild(div);
+        })
+    }
+    return { array, renderGameBoard }; 
+})();
+
+
+const game = (() => {
+    
+    restart.addEventListener('click', () => {
+        window.location = window.location;
+    });
+})()
+
+gameBoard.renderGameBoard();
