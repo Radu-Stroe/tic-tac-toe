a46572e9cd401f57f63dec605f1d0d9bab2ff079
Radu <stroeradu03@gmail.com>
2022-01-05 18:24:40 +0200

    Implement game winner functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const board = document.querySelector('#board');
 const restart = document.querySelector('.restart');
+const message = document.querySelector('#message p');
 
 let playerFlag = true;
 
@@ -55,6 +56,7 @@ let gameBoard = (() => {
                     array[array.indexOf(element)].symbol = secondPlayer.symbol;
                     playerFlag = true;
                 }
+                game.gameWinner();
             }, {once : true})
             board.appendChild(div);
         })
@@ -64,10 +66,35 @@ let gameBoard = (() => {
 
 
 const game = (() => {
-    
+    const gameWinner = () => {
+        if (gameBoard.array[0].symbol === 'X' && gameBoard.array[1].symbol === 'X' && gameBoard.array[2].symbol === 'X' || 
+            gameBoard.array[3].symbol === 'X' && gameBoard.array[4].symbol === 'X' && gameBoard.array[5].symbol === 'X' || 
+            gameBoard.array[6].symbol === 'X' && gameBoard.array[7].symbol === 'X' && gameBoard.array[8].symbol === 'X' || 
+            gameBoard.array[0].symbol === 'X' && gameBoard.array[3].symbol === 'X' && gameBoard.array[6].symbol === 'X' || 
+            gameBoard.array[1].symbol === 'X' && gameBoard.array[4].symbol === 'X' && gameBoard.array[7].symbol === 'X' || 
+            gameBoard.array[2].symbol === 'X' && gameBoard.array[5].symbol === 'X' && gameBoard.array[8].symbol === 'X' || 
+            gameBoard.array[0].symbol === 'X' && gameBoard.array[4].symbol === 'X' && gameBoard.array[8].symbol === 'X' || 
+            gameBoard.array[2].symbol === 'X' && gameBoard.array[4].symbol === 'X' && gameBoard.array[6].symbol === 'X'
+        ) {
+            message.textContent = 'Player 1 won !';
+        } else if (
+            gameBoard.array[0].symbol === 'O' && gameBoard.array[1].symbol === 'O' && gameBoard.array[2].symbol === 'O' || 
+            gameBoard.array[3].symbol === 'O' && gameBoard.array[4].symbol === 'O' && gameBoard.array[5].symbol === 'O' || 
+            gameBoard.array[6].symbol === 'O' && gameBoard.array[7].symbol === 'O' && gameBoard.array[8].symbol === 'O' || 
+            gameBoard.array[0].symbol === 'O' && gameBoard.array[3].symbol === 'O' && gameBoard.array[6].symbol === 'O' || 
+            gameBoard.array[1].symbol === 'O' && gameBoard.array[4].symbol === 'O' && gameBoard.array[7].symbol === 'O' || 
+            gameBoard.array[2].symbol === 'O' && gameBoard.array[5].symbol === 'O' && gameBoard.array[8].symbol === 'O' || 
+            gameBoard.array[0].symbol === 'O' && gameBoard.array[4].symbol === 'O' && gameBoard.array[8].symbol === 'O' || 
+            gameBoard.array[2].symbol === 'O' && gameBoard.array[4].symbol === 'O' && gameBoard.array[6].symbol === 'O'
+        ) {
+            message.textContent = 'Player 2 won !'
+        }
+    }
+
     restart.addEventListener('click', () => {
         window.location = window.location;
     });
+    return { gameWinner };
 })()
 
 gameBoard.renderGameBoard();
