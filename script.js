0d4ae54408f474e19bb98bb438fb12c6973403cd
Radu <stroeradu03@gmail.com>
2022-01-06 21:03:50 +0200

    Implement player name and draw functionality

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const board = document.querySelector('#board');
-const restart = document.querySelector('.restart');
-const message = document.querySelector('#message p');
+const startButton = document.querySelector('.start');
+const restartButton = document.querySelector('.restart');
+const message = document.querySelector('#message h2');
+const input = document.querySelector('#message input');
+
 
 let playerFlag = true;
 
@@ -22,40 +25,43 @@ let playerFlag = true;
 // })();
 
 //PLAYERS
-let player = (symbol) => {
-    return {symbol }
+let players = (name, symbol) => {
+    return {name, symbol }
 };
-
-let firstPlayer = player('X');
-let secondPlayer = player('O');
+let player = players('', 'X');
+let computer = players('Computer', 'O');
 
 //GAMEBOARD
 let gameBoard = (() => {
     let array = [
-        {symbol: ''}, 
-        {symbol: ''}, 
-        {symbol: ''}, 
-        {symbol: ''}, 
-        {symbol: ''}, 
-        {symbol: ''}, 
-        {symbol: ''}, 
-        {symbol: ''}, 
-        {symbol: ''}
+        {marked: '', symbol: ''}, 
+        {marked: '', symbol: ''}, 
+        {marked: '', symbol: ''}, 
+        {marked: '', symbol: ''}, 
+        {marked: '', symbol: ''}, 
+        {marked: '', symbol: ''}, 
+        {marked: '', symbol: ''}, 
+        {marked: '', symbol: ''}, 
+        {marked: '', symbol: ''}
     ];
+
+
+
     const renderGameBoard = () => {
         array.forEach((element) => {
             let div = document.createElement('div');
             div.classList.add('boardElement');
             div.addEventListener('click', () => {
                 if(playerFlag) {
-                    div.textContent = firstPlayer.symbol;
-                    array[array.indexOf(element)].symbol = firstPlayer.symbol;
+                    div.textContent = player.symbol;
+                    array[array.indexOf(element)].symbol = player.symbol;
                     playerFlag = false;
                 } else {
-                    div.textContent = secondPlayer.symbol;
-                    array[array.indexOf(element)].symbol = secondPlayer.symbol;
+                    div.textContent = computer.symbol;
+                    array[array.indexOf(element)].symbol = computer.symbol;
                     playerFlag = true;
                 }
+                element.marked = true;
                 game.gameWinner();
             }, {once : true})
             board.appendChild(div);
@@ -66,6 +72,20 @@ let gameBoard = (() => {
 
 
 const game = (() => {
+    // EVENT LISTENERS
+    startButton.addEventListener('click', () => {
+        player.name = input.value;
+        message.textContent = '';
+        message.style.margin = '50px';
+        input.style.display = 'none';
+        startButton.style.display = 'none';
+        gameBoard.renderGameBoard();
+    })
+    restartButton.addEventListener('click', () => {
+        window.location = window.location;
+    });
+
+
     const gameWinner = () => {
         if (gameBoard.array[0].symbol === 'X' && gameBoard.array[1].symbol === 'X' && gameBoard.array[2].symbol === 'X' || 
             gameBoard.array[3].symbol === 'X' && gameBoard.array[4].symbol === 'X' && gameBoard.array[5].symbol === 'X' || 
@@ -76,7 +96,7 @@ const game = (() => {
             gameBoard.array[0].symbol === 'X' && gameBoard.array[4].symbol === 'X' && gameBoard.array[8].symbol === 'X' || 
             gameBoard.array[2].symbol === 'X' && gameBoard.array[4].symbol === 'X' && gameBoard.array[6].symbol === 'X'
         ) {
-            message.textContent = 'Player 1 won !';
+            message.textContent = `${player.name} won!`;
         } else if (
             gameBoard.array[0].symbol === 'O' && gameBoard.array[1].symbol === 'O' && gameBoard.array[2].symbol === 'O' || 
             gameBoard.array[3].symbol === 'O' && gameBoard.array[4].symbol === 'O' && gameBoard.array[5].symbol === 'O' || 
@@ -87,14 +107,15 @@ const game = (() => {
             gameBoard.array[0].symbol === 'O' && gameBoard.array[4].symbol === 'O' && gameBoard.array[8].symbol === 'O' || 
             gameBoard.array[2].symbol === 'O' && gameBoard.array[4].symbol === 'O' && gameBoard.array[6].symbol === 'O'
         ) {
-            message.textContent = 'Player 2 won !'
+            message.textContent = `${computer.name} won!`;
+        } else if (
+            gameBoard.array[0].marked && gameBoard.array[1].marked && gameBoard.array[2].marked && 
+            gameBoard.array[3].marked && gameBoard.array[4].marked && gameBoard.array[5].marked && 
+            gameBoard.array[6].marked && gameBoard.array[7].marked && gameBoard.array[8].marked
+        ) {
+            message.textContent = `It's a draw!`;
         }
     }
 
-    restart.addEventListener('click', () => {
-        window.location = window.location;
-    });
     return { gameWinner };
 })()
-
-gameBoard.renderGameBoard();
